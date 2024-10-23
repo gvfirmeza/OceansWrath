073c3710a31b4b442f022cb80ab87f86a6b7f422
gvfirmeza <gvfirmeza@gmail.com>
2024-10-22 21:18:40 -0300

    feat: conectando com db

--- oceans-wrath/global.gd
+++ oceans-wrath/global.gd
@@ -2,3 +2,13 @@ extends Node
 
 var coins := 0
 var old_coins := 0
+
+func _ready() -> void:
+	SilentWolf.configure({
+		"api_key": "DORbSnCKsgaRaPQOf5qKL6IQv1giTYP98X4WtfHt",
+		"game_id": "oceanswrath",
+		"log_level": 1
+	})
+	SilentWolf.configure_scores({
+		"open_scene_on_close": "res://scenes/Principal.tscn"
+	})
--- oceans-wrath/menu_scene.gd
+++ oceans-wrath/menu_scene.gd
@@ -1,8 +1,19 @@
 extends Node2D
 
+var score = 6969
+var player_name = "Loka"
+
+
 func _on_play_button_pressed() -> void:
 	get_parent().mudar_cena(self, get_parent().get_node("GameScene"))
 	$AudioStreamPlayer2D.stream_paused = true;
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_teste_pressed() -> void:
+	var sw_result: Dictionary = await SilentWolf.Scores.save_score(player_name, score).sw_save_score_complete
+	print("Score persisted successfully: " + str(sw_result.score_id))
+	
+	var sw_scores: Dictionary = await SilentWolf.Scores.get_scores().sw_get_scores_complete
+	print("Scores: " + str(sw_scores.scores))
